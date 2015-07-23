66e4bec5bd1caec4bbe35d54fc1361359a55b387
smmaiolo <samantha.maiolo93@gmail.com>
2015-07-23 02:24:28 +0200

    Aggiunte altre funzioni base

--- Lisp/compgeo.lisp
+++ Lisp/compgeo.lisp
@@ -53,13 +53,11 @@
 		((null b)nil)
 		(t(+ (* (x a) (x b))
 			 (* (y a) (y b))))))
-
 ; Crea la funzione norm 
 ( defun norm (a)
  	(cond ((null  a) nil)
  		  (t(sqrt(+(*(x a) (x a))
  		  	      (* (y a) (y a)))))))
-
 
 ; utiliziamo il prodotto e le norme per calcolarci il coseno dell angolo
 ; per trovare il valore dell angolo in radianti utilizziamo l arcoseno
@@ -68,5 +66,46 @@
     	  ((null b) nil)
     	  (t(acos(/(prodScalare a b)
 	               (* (norm a) (norm b)))))))
-	
- 		  	   
+
+
+; funzione che data come input una lista di listeordina tramite la sort
+; rispetto al primo membro di ogni lista	
+ (defun ordinax (lista_punti)
+ 	(cond ((null lista_punti)nil)
+ 		  (t (sort lista_punti #'< :key #'first))))		
+; funzione che data come input una lista di listeordina tramite la sort
+; rispetto al secondo membro di ogni lista	
+ (defun ordinay (lista_punti)
+ 	(cond ((null lista_punti)nil)
+ 		  (t (sort lista_punti #'< :key #'second))))
+ ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; operazioni sulle liste
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;funione che dati un elemento (una lista di due elementi) e una lista 
+; aggiunge l'elemento alla lista
+; l'aggiunta avviene in testa
+(defun inserisci_ele ( Ele lista)
+	(cond ((null Ele)nil)
+		  (t(push Ele lista))))		  	  	   
+
+; funzione che data una lista elimina l elemento in testa alla lista
+; infatti il risultato è l elemento eliminato
+(defun elimina_ele (lista)
+	(cond ((null lista)nil)
+	      (t(pop lista))))	
+
+; dato in ingresso (rest lista) e il primo elemento
+; cerca il minimo di una lista
+(defun cerca_min (lista ele-min)
+	(cond ((null (car lista)) ele-min)	;caso base: ho un solo elemento
+										;restituisco l'elemento
+        ((< (car lista) ele-min) (cerca_min (rest lista) (car lista)))	;se l'elemento in testa è minore
+        																	;dell'elemento min attuale, passo ricorsivo
+        																	;cerca_minimo (rest list) (first list)
+        (t (cerca_min (cdr lista) ele-min))
+    )
+)
+
+
+
