80a5a12d3f782dee2388e7218f8d9b9b605f0182
ccarcione <carcione.christian@gmail.com>
2015-08-01 00:26:32 +0200

    Tolti predicati non necessari.

--- Prolog/compgeo.pl
+++ Prolog/compgeo.pl
@@ -37,21 +37,6 @@ is-collinear([Ax, Ay], [Bx, By], [Cx, Cy]):-
     area2([Ax, Ay], [Bx, By], [Cx, Cy], Area),
     Area == 0.
 
-% Predicato per il calcolo del prodotto scalare
-scalarProduct([Ax, Ay], [Bx, By], Num):- 
-    P1 is (Ax * Bx),
-    P2 is (Ay * By),
-    Num is P1 + P2.
-
-% Predicato di supporto per la norma
-norm1([V], Ris):- Ris is (V * V), !.
-norm1([V|Vs], Ris):-
-	norm1(Vs, PSol),
-	Ris is (V * V) + PSol.
-
-% Ris contiene la norma di V
-norm(V, Ris):- norm1(V, PSol), Ris is sqrt(PSol).
-
 % Dati due punti angle2d calcola l'angolo polare
 angle2d(A, B, R):- y(B, Yb),
     y(A, Ya),
